b53353dac016147243b4146ba59b4576626c494b
mattzech <matthew.zech@marquette.edu>
2021-04-20 12:39:11 -0500

    news reel

---
+++ app/src/main/java/com/example/diseaseadvisor/africa.java
@@ -0,0 +1,14 @@
+package com.example.diseaseadvisor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class africa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_africa);
+    }
+}
